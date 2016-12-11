4ef648a3980757b83358594c2ead5b5d176855b2
bingoogolapple <bingoogolapple@gmail.com>
2016-12-11 11:51:32 +0800

    「update」修改 Snackbar 笔记

--- app/src/main/java/cn/bingoogolapple/materialdesignsupportnote/util/SnackbarUtil.java
+++ app/src/main/java/cn/bingoogolapple/materialdesignsupportnote/util/SnackbarUtil.java
@@ -1,7 +1,15 @@
 package cn.bingoogolapple.materialdesignsupportnote.util;
 
+import android.graphics.Color;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cn.bingoogolapple.materialdesignsupportnote.R;
 
 /**
  * 作者:王浩 邮件:bingoogolapple@gmail.com
@@ -14,10 +22,36 @@ public class SnackbarUtil {
     }
 
     public static void show(View view, CharSequence text) {
-        if (text.length() < 10) {
-            Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show();
-        } else {
-            Snackbar.make(view, text, Snackbar.LENGTH_LONG).show();
-        }
+        Snackbar snackbar = Snackbar.make(view, text, text.length() < 10 ? Snackbar.LENGTH_SHORT : Snackbar.LENGTH_LONG)
+                .setAction("Action", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(v.getContext(), "点击了 Action", Toast.LENGTH_SHORT).show();
+                    }
+                }).setActionTextColor(Color.parseColor("#00ffff"))
+                .setCallback(new Snackbar.Callback() {
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        Toast.makeText(snackbar.getView().getContext(), "消失", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onShown(Snackbar snackbar) {
+                        Toast.makeText(snackbar.getView().getContext(), "显示", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER_VERTICAL;
+
+        ImageView number1Iv = new ImageView(view.getContext());
+        number1Iv.setImageResource(R.mipmap.number_1);
+
+        snackbarLayout.addView(number1Iv, 0, layoutParams);
+        ((TextView) snackbarLayout.findViewById(android.support.design.R.id.snackbar_text)).setTextColor(Color.parseColor("#ffaaaa"));
+        snackbarLayout.setBackgroundColor(Color.parseColor("#aaaaff"));
+
+        snackbar.show();
     }
 }
